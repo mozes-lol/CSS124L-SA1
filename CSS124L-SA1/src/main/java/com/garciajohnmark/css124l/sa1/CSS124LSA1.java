95ddd4603e392a702ef7a11b5b48f084487f50e7
mozes-lol <johnmarkgarcia164@gmail.com>
2025-02-04 18:58:57 +0800

    Create project

---
+++ CSS124L-SA1/src/main/java/com/garciajohnmark/css124l/sa1/CSS124LSA1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.garciajohnmark.css124l.sa1;
+
+/**
+ *
+ * @author John Mark Garcia
+ */
+public class CSS124LSA1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
